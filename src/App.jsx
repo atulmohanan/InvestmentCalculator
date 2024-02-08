ecd9ea57d34b944731a51c09d2a719ff8ec4e01d
Atul Mohanan <107179770+atulmohanan@users.noreply.github.com>
2024-02-08 15:50:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Header from './components/Header';
+import Results from './components/Results.jsx';
+import UserInput from './components/UserInput';
+import { useState } from 'react';
+
+function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  const inputIsValid = userInput.duration >= 1;
+
+  return (
+    <>
+      <Header/>
+      <UserInput userInput= {userInput} onChange={handleChange}/>
+      {!inputIsValid && (<p className="center"> Please enter a duration of greater than zero</p>)}
+      {inputIsValid && <Results input={userInput}/>}
+    </>
+    );
+}
+
+export default App
